5064a7feb8ef53eadf9132cbd348867ca921779c
Good Developer <gooddeveloper2004@gmail.com>
2023-03-13 15:30:36 +0500

    add init codes for html and js

---
+++ main.js
@@ -0,0 +1,14 @@
+const timeInput = document.getElementById('time');
+const stepInput = document.getElementById('step');
+const incrementBtn = document.getElementById('increment');
+const decrementBtn = document.getElementById('decrement');
+
+
+
+
+
+
+
+
+
+
